e034217c54e1917fa7385fd2677a07e35fc93b52
7amedalhashmi <7alhashmi@gmail.com>
2022-03-15 17:32:23 +0400

    updating

--- client.c
+++ client.c
@@ -1,6 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: halhashm <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/15 16:47:27 by halhashm          #+#    #+#             */
+/*   Updated: 2022/03/15 17:13:11 by halhashm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <signal.h>
 #include <unistd.h>
-#include <stdio.h>
 #include <stdlib.h>
 
 void	ft_error(char *error)
@@ -39,18 +50,18 @@ int	ft_atoi(const char *str)
 	return ((int) num * sign);
 }
 
-int errHandler(int pid, char *str)
+int	errhandler(int pid, char *str)
 {
 	if (pid <= 1000 || pid >= 100000)
 		ft_error("Invalid PID number\n");
 	else if (*str == '\0')
 		ft_error("Nothing to send\n");
-	return(1);
+	return (1);
 }
 
-void sigToKill(pid_t pid, char *str)
+void	sigtokill(pid_t pid, char *str)
 {
-	int i;
+	int	i;
 
 	while (*str != '\0')
 	{
@@ -62,7 +73,7 @@ void sigToKill(pid_t pid, char *str)
 				if (kill(pid, SIGUSR1) == -1)
 					ft_error("Invalid PID\n");
 			}
-            else if ((*str >> i & 1) == 1)
+			else if ((*str >> i & 1) == 1)
 			{
 				if (kill(pid, SIGUSR2) == -1)
 					ft_error("Invalid PID\n");
@@ -74,12 +85,12 @@ void sigToKill(pid_t pid, char *str)
 	}
 }
 
-int main(int arc, char **arg)
+int	main(int arc, char **arg)
 {
 	if (arc != 3)
 		ft_error("Invalid number of arguments\n");
-	else if (errHandler(ft_atoi(arg[1]), arg[2]) == 0)
-		return(0);
-	sigToKill(ft_atoi(arg[1]), arg[2]);
-	return(0);
+	else if (errhandler(ft_atoi(arg[1]), arg[2]) == 0)
+		return (0);
+	sigtokill(ft_atoi(arg[1]), arg[2]);
+	return (0);
 }
--- server.c
+++ server.c
@@ -1,5 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: halhashm <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/15 16:47:08 by halhashm          #+#    #+#             */
+/*   Updated: 2022/03/15 17:12:18 by halhashm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <signal.h>
-#include <stdio.h>
 #include <unistd.h>
 
 void	ft_putchar(char c)
@@ -7,7 +18,7 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-void		ft_putnbr(int c)
+void	ft_putnbr(int c)
 {
 	unsigned int	i;
 
@@ -22,39 +33,34 @@ void		ft_putnbr(int c)
 	ft_putchar(i % 10 + 48);
 }
 
-void ft_signal(int sig)
+void	ft_signal(int sig)
 {
-	static int i;
-	static char str;
+	static int	i;
+	static char	str;
 
 	i++;
-    if (sig == SIGUSR2)
-	{
+	str <<= 1;
+	if (sig == SIGUSR2)
 		str |= 1;
-	}
 	if (i == 8)
 	{
 		ft_putchar(str);
 		i = 0;
 		str = 0;
 	}
-	else
-	{
-		str <<= 1;
-	}
 }
 
-int main()
+int	main(void)
 {
-    pid_t pid;
+	pid_t	pid;
 
 	pid = getpid();
 	write(1, "\nThe PID number is : ", 22);
 	ft_putnbr(pid);
 	ft_putchar('\n');
-    signal(SIGUSR1, ft_signal);
+	signal(SIGUSR1, ft_signal);
 	signal(SIGUSR2, ft_signal);
-    while (1)
-        pause();
-    return 0;
+	while (1)
+		pause();
+	return (0);
 }
